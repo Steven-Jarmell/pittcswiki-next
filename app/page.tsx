0dd71a42f469d684e91f347fedd6bf5d9a7370b1
Steven Jarmell <sjj27@pitt.edu>
2023-10-31 19:41:11 -0400

    Add old CSS globals to tailwind and initialize header component

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
-import Image from 'next/image'
+import Header from "@/components/Header"
+import Image from "next/image"
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      
+    <main className="">
+      <Header />
     </main>
   )
 }
---
+++ components/Header.tsx
@@ -0,0 +1,39 @@
+/**
+ * Header Component
+ *
+ * @author varughese
+ * @author Steven-Jarmell
+ */
+
+import Link from "next/link"
+import Image from "next/image"
+
+import logo from "../images/pantherhandbook-mini.svg"
+
+const Header = (): JSX.Element => {
+  return (
+    <header className="flex flex-none h-16 px-8 mb-4 bg-pittblue pwiki-header">
+      <div className="flex items-center flex-1 justify-between">
+        <div className="flex items-center">
+          <Link
+            className="header-logo flex font-semibold text-white items-center"
+            href="/"
+          >
+            <Image
+              src={logo}
+              alt=""
+              className="w-10 md:mr-0 mr-4 max-w-[50px]"
+            />
+          </Link>
+        </div>
+      </div>
+      <div className="w-full md:w-1/3">
+        <div className="relative">
+          <input type="text" placeholder="Temporary Search Bar" />
+        </div>
+      </div>
+    </header>
+  )
+}
+
+export default Header
