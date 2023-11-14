eb4889240d08d785ee4730e392c0dca287089715
Steven Jarmell <sjj27@pitt.edu>
2023-11-14 16:10:51 -0500

    Finish initial main page

--- app/page.tsx
+++ app/page.tsx
@@ -10,10 +10,14 @@ export default function Home() {
   return (
     <main className="">
       <div className="landing-image">
-        <Image alt="" src={CampusBackground} className="w-auto md:w-full" />
+        <Image
+          alt="Picture of Pitt's Campus as a background image"
+          src={CampusBackground}
+          className="w-auto md:w-full"
+        />
       </div>
       <div className="max-w-4xl mx-auto">
-        <div className="md:mt-24 flex">
+        <div className="md:mt-24 flex justify-center items-center">
           <div className="hidden md:block self-end h-64 bg-white p-6 shadow-2xl w-1/2 border">
             <h1>The Pitt CS Wiki</h1>
             <p>
@@ -30,12 +34,12 @@ export default function Home() {
           <div className="w-1/2">
             <Image
               src={Logo}
-              className="ml-auto max-w-sm -mb-12"
+              className="ml-auto max-w-sm -mb-12 w-auto"
               alt="Pitt CS Wiki Logo"
             />
           </div>
         </div>
-        <div className="md:hidden mt-12">
+        <div className="md:hidden mt-12 text-center">
           <h1>The Pitt CS Wiki</h1>
           <p>
             A collection of course testimonials, guides, career prep resources,
@@ -43,7 +47,7 @@ export default function Home() {
           </p>
           <Link
             href="/guides/"
-            className="btn inline-flex bg-pittgold items-center"
+            className="btn inline-flex bg-pittgold items-center font-semibold py-2 px-2 border rounded"
           >
             Get Started {RightArrowSvg}
           </Link>
