54ec269d7c34c0764b60b4b0923be70ea8b5224a
Steven Jarmell <sjj27@pitt.edu>
2023-11-21 11:37:14 -0500

    Add cool 404 page

---
+++ app/not-found.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image"
+import RocPic from "@/app/_images/roc.png"
+
+export default function Custom404() {
+  return (
+    <div className="flex flex-col justify-center items-center">
+        <h1 className="text-center">Much like our quarterback, this page does not exist</h1>
+      <Image src={RocPic} alt="Picture of Roc <3" className="w-[80vw] sm:w-[70vw] lg:w-[40vw]" />
+    </div>
+  )
+}
