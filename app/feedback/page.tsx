4d468dea8b01c163a99c891d51e29c80d297f44a
Steven Jarmell <sjj27@pitt.edu>
2023-12-07 17:01:25 -0500

    Add feedback page

--- app/feedback/page.tsx
+++ app/feedback/page.tsx
@@ -1,3 +1,12 @@
 export default function FeedbackPage() {
-  return <div>Feedback Page</div>
+  return <div>
+  <iframe
+    title="Feedback and Q&A"
+    src="https://docs.google.com/forms/d/e/1FAIpQLSfijKV1sHF7QGWYc6UzIbUuIIntDOPbyqdrzXg-snHeBN_qNg/viewform?embedded=true"
+    width="100%"
+    height="800"
+  >
+    Loading…
+  </iframe>
+</div>
 }
